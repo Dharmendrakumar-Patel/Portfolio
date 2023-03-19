d8365c3cfc4b6ac9ad80aa3332463e4ce838961c
Dharmendrakumar-Patel <pateldharmendrakumarsalamsinh@yahoo.com>
2023-03-20 02:14:31 +0530

    fixed site crash issue.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,3 @@
-import React, {useState, useEffect} from 'react'
-import { Link } from 'react-router-dom'
-import { Styles } from '../styles'
-import { navlinks } from '../constants'
-import { logo, menu, close } from '../assets'
-
 const Navbar = () => {
   return (
     <div>Navbar</div>

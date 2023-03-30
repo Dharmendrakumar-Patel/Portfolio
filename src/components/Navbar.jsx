8c1a92ac0caa5b15a5a7c6dd5d955a7a1444d8f5
Dharmendrakumar-Patel <pateldharmendrakumarsalamsinh@yahoo.com>
2023-03-30 23:56:47 +0530

    added custom logo.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,30 @@
+import React, {useState} from 'react'
+import {styles} from '../styles'
+import {Link} from 'react-router-dom'
+import logo from '../assets/logo.svg'
+
 const Navbar = () => {
+  const [active, setActive] = React.useState('')
   return (
-    <div>Navbar</div>
+    <nav
+      className={`
+        ${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary
+      `}
+    >
+      <div className="w-full flex items-center justify-items-center max-w-7xl mx-auto">
+        <Link 
+          to='/'
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive('');
+            window.scrollTo(0, 0)
+          }
+          }
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+        </Link>
+      </div>
+    </nav>
   )
 }
 
